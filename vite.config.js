4a3addcbe351122217093f35a1a31d4667e86e32
Gaurav Sobti <gauravsobti1@gmail.com>
2024-02-12 17:51:46 +1100

    changed based in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/categorize-budget",
   css: {
     postcss: {
       plugins: [tailwindcss()],
